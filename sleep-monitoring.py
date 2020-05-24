cfcfbc0e181452a99901b136f1e4f10e7ed5d0a3
Pascal Horton <horton.pascal@gmail.com>
2020-05-24 23:47:09 +0200

    Moving sensors code to a new file.

--- sleep-monitoring.py
+++ sleep-monitoring.py
@@ -1,12 +1,7 @@
-import math
-import sys
 import time
 import datetime
-from grove.adc import ADC
-from grove.button import Button
-from grove.factory import Factory
-from grove.gpio import GPIO
-import seeed_dht
+from sensors import *
+
 
 slot_loudness_sensor = 6
 slot_proximity_sensor = 0
@@ -22,180 +17,6 @@ use_temp_humidity_sensor = True
 use_button = True
 
 debug = False
-
-
-class GroveTemperatureHumiditySensor:
-
-    def __init__(self, channel):
-        self.__sensor = seeed_dht.DHT("22", 12)
-        self.__humidity_sum = 0
-        self.__humidity_count = 0
-        self.__temperature_sum = 0
-        self.__temperature_count = 0
-
-    @property
-    def humidity(self):
-        humidity, temp = self.__sensor.read()
-        return humidity
-
-    @property
-    def temperature(self):
-        humidity, temp = self.__sensor.read()
-        return temp
-
-    @property
-    def temperature_and_humidity(self):
-        return self.__sensor.read()
-
-    def record_value(self):
-        humidity, temp = self.__sensor.read()
-        self.__humidity_sum += humidity
-        self.__humidity_count += 1
-        self.__temperature_sum += temp
-        self.__temperature_count += 1
-
-    def reset_records(self):
-        self.__humidity_sum = 0
-        self.__humidity_count = 0
-        self.__temperature_sum = 0
-        self.__temperature_count = 0
-
-
-class GroveLoudnessSensor:
-
-    def __init__(self, channel):
-        self.__channel = channel
-        self.__adc = ADC()
-        self.__sum = 0
-        self.__count = 0
-        self.__max = 0
-
-    @property
-    def value(self):
-        return self.__adc.read(self.__channel)
-
-    @property
-    def max(self):
-        return self.__max
-
-    @property
-    def mean(self):
-        return self.__sum / max(self.__count, 1)
-
-    def record_value(self):
-        self.__sum += self.value
-        self.__count += 1
-        self.__max = max(self.__max, self.value)
-
-    def reset_records(self):
-        self.__sum = 0
-        self.__count = 0
-        self.__max = 0
-
-
-class GroveButton(object):
-    def __init__(self, pin):
-        # High = pressed
-        self.__btn = Factory.getButton("GPIO-HIGH", pin)
-        self.__last_time = time.time()
-        self.__pressed = 0
-        self.__btn.on_event(self, GroveButton.__handle_event)
-
-    @property
-    def pressed(self):
-        return self.__pressed
-
-    def reset_pressed(self):
-        self.__pressed = 0
-
-    def __on_press(self):
-        self.__pressed = 1
-
-    def __handle_event(self, evt):
-        dt, self.__last_time = evt["time"] - self.__last_time, evt["time"]
-        if evt["code"] == Button.EV_LEVEL_CHANGED:
-            if evt["pressed"]:
-                self.__on_press()
-
-
-class GroveInfraredProximitySensor(GPIO):
-    def __init__(self, pin):
-        super(GroveInfraredProximitySensor, self).__init__(pin, GPIO.IN)
-        self.__counter = 0
-
-    @property
-    def counter(self):
-        return self.__counter
-
-    def on_detect(self):
-        if self.on_event is None:
-            self.on_event = self.__handle_event
-        self.__counter += 1
-
-    def reset_counter(self):
-        self.__counter = 0
-
-    def __handle_event(self, pin, value):
-        if value:
-            self.on_detect()
-
-
-class GroveLed(GPIO):
-    def __init__(self, pin):
-        super(GroveLed, self).__init__(pin, GPIO.OUT)
-
-    def on(self):
-        self.write(1)
-
-    def off(self):
-        self.write(0)
-
-    def long_flash(self):
-        self.on()
-        time.sleep(3)
-        self.off()
-
-    def double_flash(self):
-        self.on()
-        time.sleep(.2)
-        self.off()
-        time.sleep(.2)
-        self.on()
-        time.sleep(.2)
-        self.off()
-
-
-class GroveLightSensor:
-
-    def __init__(self, channel):
-        self.__channel = channel
-        self.__adc = ADC()
-        self.__sum = 0
-        self.__count = 0
-        self.__max = 0
-
-    @property
-    def light(self):
-        value = self.__adc.read(self.__channel)
-        return value
-
-    @property
-    def max(self):
-        return self.__max
-
-    @property
-    def mean(self):
-        return self.__sum / max(self.__count, 1)
-
-    def record_value(self):
-        self.__sum += self.light
-        self.__count += 1
-        self.__max = max(self.__max, self.light)
-
-    def reset_records(self):
-        self.__sum = 0
-        self.__count = 0
-        self.__max = 0
 
 
 def main():
@@ -228,7 +49,7 @@ def main():
 
             end = time.time()
             if end - start >= 60:
-                f.write(datetime.datetime.now().isoformat())
+                f.write('{}, '.format(datetime.datetime.now().isoformat()))
                 if use_loudness_sensor:
                     f.write('{}, {}, '.format(loudness_sensor.mean, loudness_sensor.max))
                     loudness_sensor.reset_records()
@@ -253,7 +74,7 @@ def main():
 
 def create_file():
     filename = datetime.datetime.now().strftime("%Y-%m-%d_%H%M_monitoring.csv")
-    f = open(filename, 'w')
+    f = open(filename, 'w', buffering=0)
     f.write('time, ')
     if use_loudness_sensor:
         f.write('loudness_mean, loudness_max, ')
